ecfd8c631fff96137eb111412b241d788fa5e222
cykim43 <35681142+cykim43@users.noreply.github.com>
2023-06-26 15:20:02 +0900

    Update apiConfig.js

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://back-lb.app.svc';
+const BASE_URL = 'https://back-lb.app.svc';
 
 export const API_ADDRESS = {
   product_main: `${BASE_URL}/products?region=seoul&?offset=1&limit=5`,
