21f44d74960a2c46867caae5c9042bbaad29d4e8
cykim43 <35681142+cykim43@users.noreply.github.com>
2023-06-27 12:30:59 +0900

    Update apiConfig.js

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://172.20.59.211:8000';
+const BASE_URL = 'http://back-svc.app.svc';
 
 export const API_ADDRESS = {
   product_main: `${BASE_URL}/products?region=seoul&?offset=1&limit=5`,
